d915d7d9732af69eeae41702f9bb77f823e535ea
Kyriakos Zantis <kkzantis@hotmail.com>
2016-04-24 19:52:19 +0300

    Menu Change:

--- 446/src/OurMain.java
+++ 446/src/OurMain.java
@@ -4,8 +4,8 @@ import Interface.Menu;
 
 public class OurMain extends Menu {
 
-	static int num_of_clients = 3;
-	static Thread[] clients = new Thread[num_of_clients];
+	
+	Thread[] clients = new Thread[num_of_clients];
 	static ArrayList<Item> items = new ArrayList<Item>(20);
 	/**
 	 * @param args
@@ -16,6 +16,44 @@ public class OurMain extends Menu {
 
 		String inputfile;
 		String name;
+
+		switch(option_update){
+			case 1:{
+					
+					System.out.println("The Update is  Immediate");
+					break;
+					}
+			 case 2:{
+				
+				System.out.println("The Update is  Deferred");
+				break;
+				}
+			 default:{System.out.println("ERROR.You didnt Choose");}
+		}
+		
+		switch(option_type){
+		case 1:{
+				
+				System.out.println("The Type is  Wound and wait");
+				break;
+				}
+		 case 2:{
+			
+			System.out.println("The Type is  Wait And Die");
+			break;
+			}
+		 case 3:{
+				
+			System.out.println("The Type is  Cautious Waiting");
+			break;
+			}
+		 default:{System.out.println("ERROR.You didnt Choose");}
+		
+	}
+		
+		System.out.println("The Number of Clients are "+num_of_clients);
+		
+		
 		for (int i=0; i<20; i++)
 			items.add(new Item((char)('a'+i)));
 		Thread t1 = new Thread(new TCPServer("Server"));
@@ -24,8 +62,13 @@ public class OurMain extends Menu {
 		for (int i=0; i<num_of_clients; i++){
 			inputfile="input"+(i+1)+".txt";
 			name ="Client"+(i+1);
+			try{
 			clients[i] = new Thread(new TCPClient(inputfile, name, (i+1)));
 			clients[i].start();
+			}catch (Exception e){
+				System.out.println("There is no file with name "+inputfile );
+			}
+		
 		}
 	
 
